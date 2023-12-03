b26249a6b00d5623c0b59d202fd5c46a70712494
VishalBurra27 <vishalmathews2004@gmail.com>
2023-12-03 22:09:29 +0530

    inital

---
+++ genetic.py
@@ -0,0 +1,10 @@
+# Genetic Algorithm
+
+# gene -> (client,server) ; client-index , server-value ; data-type : 
+# chromosome -> set of genes ;                            data-type :
+# population -> set of chromosomes                        data-type :
+
+# No of servers, no. of clients
+# initialize population
+# 
+
