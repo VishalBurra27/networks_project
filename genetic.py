36faf48c2d9b65fb7b8315e013942314cedea1e0
VishalBurra27 <vishalmathews2004@gmail.com>
2023-12-03 22:17:16 +0530

    psuedo code

--- genetic.py
+++ genetic.py
@@ -1,10 +1,19 @@
 # Genetic Algorithm
 
-# gene -> (client,server) ; client-index , server-value ; data-type : 
-# chromosome -> set of genes ;                            data-type :
+# gene -> (client,server) ; client-index , server-value ; 
+# 4- attributes: {client capacity, MIPS, current server load, maximum server capacity}
+#data-type : 
+
+# chromosome -> set of genes ;                           data-type : 
+
 # population -> set of chromosomes                        data-type :
 
 # No of servers, no. of clients
 # initialize population
-# 
+# Find fitness value for all the chromosomes in the population
+# Select 2 at random and cross-over to produce a child
+# check if the child has better fitness or not -> if yes the add it to population -> else add initial child
+# return the chromosome with best fitness value
+
+
 
